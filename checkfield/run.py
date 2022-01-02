dba8206dde205bc7e58722cdb13de6ed7f1448fa
Kento Masuda <kmasuda@ess.sci.osaka-u.ac.jp>
2022-01-02 20:21:17 +0900

    cleanup

--- checkfield/run.py
+++ checkfield/run.py
@@ -1,6 +1,14 @@
 #%%
 import astropy.units as u
-from checkfield import check_field
+from otofu.obsplan import check_field
+
+#%%
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set(style='ticks', font_scale=1.6*0.5, font='sans-serif')
+plt.rcParams["figure.figsize"] = (14*0.5,7*0.5)
+from matplotlib import rc
+rc('text', usetex=False)
 
 #%%
 fov = 0.45*u.deg
@@ -9,5 +17,5 @@ fov = 0.45*u.deg
 names = ["TOI-700", "LTT 1445A", "TRAPPIST-1"]
 
 #%%
-for name in names:
+for name in names[:1]:
     coord, d = check_field(name, fov, plot=True)
