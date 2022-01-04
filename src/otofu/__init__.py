ce0cb4fe140473205dde44a43dd9f677c35edf41
Kento Masuda <k_masuda@dhcp184.ess.sci.osaka-u.ac.jp>
2022-01-04 10:23:18 +0900

    setup

---
+++ src/otofu/__init__.py
@@ -0,0 +1,11 @@
+__all__ = []
+
+__version__ = "0.0.1"
+__uri__ = "none"
+__author__ = "Kento Masuda"
+__email__ = ""
+__license__ = "MIT"
+__description__ = "observing tools for UCDs"
+
+from . import obsplan
+from . import jasmine
